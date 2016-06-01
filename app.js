b7b4aff16156821205b0ff7e73d4dc6175b7edb1
Tyler Curtis <tcurtis@vmware.com>
2016-05-31 20:21:35 -0700

    Add express and pug (jade)

---
+++ app.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+var express = require('express')
+  , logger = require('morgan')
+  , app = express()
+  , template = require('pug').compileFile(__dirname + '/templates/index.pug');
+
+app.use(logger('dev'))
+app.use(express.static(__dirname + '/static'))
+
+app.get('/', function (req, res, next) {
+  try {
+    var html = template({ title: 'Curtis HomeSmarts' })
+    res.send(html)
+  } catch (e) {
+    next(e)
+  }
+})
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
+})
